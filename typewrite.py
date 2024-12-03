498cd5fd9796b7acc0669c98401b30413a9c920b
Evan-pixelperfect <evan2401tm@gmail.com>
2024-12-03 10:26:29 +1000

    Updated typewrite to reflect some of the feedback. Will add more later

--- typewrite.py
+++ typewrite.py
@@ -1,10 +1,12 @@
 # Importing the time module for controlling the speed of the typewriter effect
 import time
 
-# Typewriter function -- The purpose of this function is to simulate a typewriter effect by printing each character of the input string one at a time with a slight delay between characters. This can be used to display text gradually, mimicking the effect of someone typing.
+"""
+Typewriter function -- The purpose of this function is to simulate a typewriter effect by printing each character of the input string one at a time with a slight delay between characters. This can be used to display text gradually, mimicking the effect of someone typing.
+"""
 
 def typewrite(string):
-# Parameters: string (str): The string to be printed with the typewriter effect. This is the input to the function.
+# Parameter: string (str): The string to be printed with the typewriter effect. This is the input to the function.
 
     # Convert the input string into a list of characters
     liststring = list(string)
